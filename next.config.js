26786ea73a973d5a7e128c51c3314df0b1350573
Anant Ojha <anantojha@cmail.carleton.ca>
2022-05-10 14:30:12 -0400

    deploy fix

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+    }
+  },
+  assetPrefix: !debug ? 'https://anotherplanet-io.github.io/Next-React-Components/' : '',
+}
+
+
