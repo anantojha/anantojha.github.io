3a166f52d99a9e554c69c25c86f33206ab4ef5a4
Anant Ojha <anantojha@cmail.carleton.ca>
2022-05-10 16:08:31 -0400

    Updates

--- components/contact.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Contact: React.FC = () => {
-    return <div className='flex flex-col bg-white py-20 text-3xl md:text-4xl'>
-                contact me
-            </div>
-}
-
-export default Contact 
