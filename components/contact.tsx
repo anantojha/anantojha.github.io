c22ecde734f1ed7be43e4b20b565804640635e43
Anant Ojha <anantojha@cmail.carleton.ca>
2022-05-11 03:05:08 -0400

    added contact button

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Contact: React.FC = () => {
-    return <div className='bg-white text-black flex flex-col justify-center items-center py-10'>
-            <div className="flex flex-col bg-white text-3xl md:text-4xl">
-                <h2 >
-                Contact
-            </h2>
-            </div>
+
+    function handleEmailSending(){
+
+    }
+
+    return <div className='bg-white text-black flex flex-col justify-center items-center '>
+        <p className="font-bold mt-20">contact by email</p>
             <form>
                 <input 
                     id="Name" 
@@ -16,7 +17,7 @@ const Contact: React.FC = () => {
                     placeholder="Name"
                     size={50}
                     type="text"
-                    className="flex flex-col bg-white text-black text-left outline-none border-2 border-black mt-4 mb-4 rounded-3xl px-5 py-2"
+                    className="flex flex-col bg-white text-black text-left outline-none border-2 border-black mt-2 mb-4 rounded-3xl px-5 py-2"
                 />
                 <input 
                     id="Email" 
@@ -36,9 +37,12 @@ const Contact: React.FC = () => {
                     placeholder="message"
                     rows={8}
                     cols={50}
-                    className="flex flex-col bg-white text-black text-left outline-none border-2 border-black mb-4 rounded-3xl px-5 py-2"
+                    className="flex flex-col bg-white text-black text-left outline-none border-2 border-black rounded-3xl px-5 py-2"
                 />
             </form>
+                <div className="text-center mt-10">
+                    <button type="submit" onClick={handleEmailSending} className="bg-black border-black hover:bg-opacity-50 active:bg-opacity-90 bg-opacity-10 text-black rounded-3xl border-2 px-16 py-2 mb-20">Send</button>
+                </div>
         </div>
           
 }
