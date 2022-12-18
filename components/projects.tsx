caac5beb29c686b09eda44d6f812a5e9d4811dd2
Anant Ojha <anantojha@cmail.carleton.ca>
2022-12-18 15:17:07 -0500

    added code

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,33 +10,48 @@ const Timeline: React.FC = () => {
         <div className={styles.projecttitle}><h1>Projects</h1></div>
         <div className="flex flex-row items-center justify-center">
             <div className={styles.gridcard}>
-                <div onClick={() => window.open("https://github.com/anantojha/JobArc", "_blank")} className="border rounded-2xl py-5 font-bold bg-black">
-                    <a className={styles.projectname} href="https://github.com/anantojha/JobArc" target="_blank">JobArc Android</a>
-                    <div className="flex flex-col items-center justify-center py-5">
-                        <img className={ styles.projectpic} src="/assets/Jobarc.png" width={500} height={500} />
+                <div onClick={() => window.open("https://github.com/anantojha/JobArc", "_blank")} className=" font-bold">
+                    <div className={styles.projectbox}>
+                        <a className={styles.projectJobArc} href="https://github.com/anantojha/JobArc" target="_blank">JobArc Android</a>
+                        <div className="flex flex-col items-center justify-center py-5">
+                            <img className={ styles.projectpic} src="/assets/Jobarc.png" width={500} height={500} />
+                        </div>
                     </div>
                 </div>
-            
             </div>
             <div className={styles.gridcard}>
-                <div onClick={() => window.open("https://github.com/anantojha/Android-Youtube-Player", "_blank")} className="border rounded-2xl">
-                    <a className={styles.projectname} href="https://github.com/anantojha/Android-Youtube-Player" target="_blank">Android Youtube Player</a>
+                <div onClick={() => window.open("https://github.com/anantojha/Android-Youtube-Player", "_blank")} className=" font-bold">
+                    <div className={styles.projectbox}>
+                        <a className={styles.projectYTPlayer} href="https://github.com/anantojha/Android-Youtube-Player" target="_blank">Android Youtube Player</a>
+                        <div className="flex flex-col items-center justify-center py-5">
+                            <video autoPlay loop muted playsInline width={162} className={styles.projectpic } >
+                                <source src="/assets/yt-portfolio-demo-fast.m4v" />
+                            </video>
+                        </div>
+                    </div>
                 </div>
-                <div ></div>
             </div>
         </div>
         <div className="flex flex-row">
             <div className={styles.gridcard}>
-                <div onClick={() => window.open("https://github.com/anantojha/Tile-Rummy", "_blank")} className="border rounded-2xl">
-                    <a className={styles.projectname} href="https://github.com/anantojha/Tile-Rummy" target="_blank">Tile Rummy Board Game</a>
+                <div onClick={() => window.open("https://github.com/anantojha/CNN-Smile-Detector", "_blank")} className=" font-bold">
+                    <div className={styles.projectbox}>
+                        <a className={styles.projectSmileDetector} href="https://github.com/anantojha/CNN-Smile-Detector" target="_blank">Real-Time Smile Detector</a>
+                        <div className="flex flex-col items-center justify-center py-5">
+                            <img className={ styles.projectpic} src="/assets/Jobarc.png" width={500} height={500} />
+                        </div>
+                    </div>
                 </div>
-                <div ></div>
             </div>
             <div className={styles.gridcard}>
-                <div onClick={() => window.open("https://github.com/anantojha/CNN-Smile-Detector", "_blank")} className="border rounded-2xl">
-                    <a className={styles.projectname} href="https://github.com/anantojha/CNN-Smile-Detector" target="_blank">Real-Time Smile Detector</a>
+                <div onClick={() => window.open("https://github.com/anantojha/Multithreaded-Elevator-System", "_blank")} className=" font-bold">
+                    <div className={styles.projectbox}>
+                        <a className={styles.projectElevator} href="https://github.com/anantojha/Multithreaded-Elevator-System" target="_blank">Multithreaded-Elevator-System</a>
+                        <div className="flex flex-col items-center justify-center py-5">
+                            <img className={ styles.projectpic} src="/assets/Jobarc.png" width={500} height={500} />
+                        </div>
+                    </div>
                 </div>
-                <div ></div>
             </div>
         </div>
         </section>
