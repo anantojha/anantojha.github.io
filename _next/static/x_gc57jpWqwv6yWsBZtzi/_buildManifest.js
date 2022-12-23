c1f302674972024c7d930660daa3355cf47a3fe3
anantojha <46407205+anantojha@users.noreply.github.com>
2022-12-23 00:57:50 +0000

    Deploying to gh-pages from  @ cc72badd9d5d274cc7eeb248c6b4bba0e142a77c 🚀

---
+++ _next/static/x_gc57jpWqwv6yWsBZtzi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/961-eae8ff381c477108.js","static/css/8ed578316b3c8ed0.css","static/chunks/pages/index-b874182aaa19a5a3.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
